ab725d4adebd5104661844f38865eed1665fe7af
Arturas Slajus <x11@arturaz.net>
2014-03-24 23:47:54 +0200

    Added parent to Component#clone.

--- Code/TLPLib/Extensions/ComponentExts.cs
+++ Code/TLPLib/Extensions/ComponentExts.cs
@@ -2,16 +2,14 @@
 
 namespace com.tinylabproductions.TLPLib.Extensions {
   public static class ComponentExts {
-    public static A clone<A>(this A self) where A : Component {
-      var cloned = (GameObject) Object.Instantiate(self.gameObject);
-      return cloned.GetComponent<A>();
-    }
-
     public static A clone<A>(
-      this A self, Vector3 position, Quaternion rotation
+      this A self, Vector3? position=null, Quaternion? rotation=null, 
+      Transform parent=null
     ) where A : Component {
-      var cloned = (GameObject) Object.
-        Instantiate(self.gameObject, position, rotation);
+      var cloned = (GameObject) Object.Instantiate(self.gameObject);
+      if (position != null) cloned.transform.position = (Vector3) position;
+      if (rotation != null) cloned.transform.rotation = (Quaternion) rotation;
+      if (parent != null) cloned.transform.parent = parent;
       return cloned.GetComponent<A>();
     }
   }
