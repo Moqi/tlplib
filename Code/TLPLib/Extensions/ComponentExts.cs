cfa7077137d8008e2d4d27beb05c842d54a16c5d
Arturas Slajus <x11@arturaz.net>
2014-03-24 19:16:09 +0200

    Added Component#clone that takes position and rotation.

--- Code/TLPLib/Extensions/ComponentExts.cs
+++ Code/TLPLib/Extensions/ComponentExts.cs
@@ -6,5 +6,13 @@ namespace com.tinylabproductions.TLPLib.Extensions {
       var cloned = (GameObject) Object.Instantiate(self.gameObject);
       return cloned.GetComponent<A>();
     }
+
+    public static A clone<A>(
+      this A self, Vector3 position, Quaternion rotation
+    ) where A : Component {
+      var cloned = (GameObject) Object.
+        Instantiate(self.gameObject, position, rotation);
+      return cloned.GetComponent<A>();
+    }
   }
 }
