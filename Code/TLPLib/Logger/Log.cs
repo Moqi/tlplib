ebcf735402748d4f37c42d706c4b41212fd325f8
Arturas Slajus <arturas@tinylabproductions.com>
2014-05-02 17:19:57 +0300

    Log class

---
+++ Code/TLPLib/Logger/Log.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Logger {
+  public static class Log {
+    public static void debug(object o) { Debug.Log(o); }
+    public static void warn(object o) { Debug.LogWarning(o); }
+    public static void error(Exception ex) { Debug.LogException(ex); }
+    public static void error(object o) { Debug.LogError(o); }
+  }
+}
