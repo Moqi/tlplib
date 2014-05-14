5e322771cd45a7b18822deeb18b3bea38f8ce4d7
Arturas Slajus <arturas@tinylabproductions.com>
2014-05-14 15:53:13 +0300

    Rect#absoluteToPercentage.

--- Code/TLPLib/Utilities/RectUtils.cs
+++ Code/TLPLib/Utilities/RectUtils.cs
@@ -2,22 +2,24 @@
 
 namespace com.tinylabproductions.TLPLib.Utilities {
   public static class RectUtils {
+    private static float sw { get { return Screen.width; } }
+    private static float sh { get { return Screen.height; } }
+
     /** Create rect that has values from percentages of screen. **/
     public static Rect percent(float left, float top, float width, float height) {
-      var sw = Screen.width;
-      var sh = Screen.height;
       return new Rect(sw * left, sh * top, sw * width, sh * height);
+    }
+
+    /** Convert absolute rect to percentage rect. **/
+    public static Rect absoluteToPercentage(this Rect pRect) {
+      return new Rect(
+        pRect.xMin / sw, pRect.yMin / sh, pRect.width / sw, pRect.height / sh
+      );
     }
 
     /** Create rect that has values from percentages of screen. **/
     public static Rect relPercent(float left, float leftEnd, float top, float topEnd) {
-      var sw = Screen.width;
-      var sh = Screen.height;
-      var absLeft = sw * left;
-      var absLeftEnd = sw * leftEnd;
-      var absTop = sh * top;
-      var absTopEnd = sh * topEnd;
-      return new Rect(absLeft, absTop, absLeftEnd - absLeft, absTopEnd - absTop);
+      return percent(left, leftEnd - left, top, topEnd - top);
     }
 
     public static Rect with(
