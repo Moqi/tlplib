b079467ff6834b9e0d76a9531399293d2cb0cbdd
Arturas Slajus <x11@arturaz.net>
2014-05-18 13:41:56 +0300

    VectorExts.

---
+++ Code/TLPLib/Extensions/VectorExts.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Extensions {
+  public static class VectorExts {
+    public static Vector2 with2(
+      this Vector2 v, float? x = null, float? y = null
+    ) {
+      return new Vector3(x ?? v.x, y ?? v.y);
+    }
+
+    public static Vector3 with3(
+      this Vector3 v, float? x = null, float? y = null, float? z = null
+    ) {
+      return new Vector3(x ?? v.x, y ?? v.y, z ?? v.z);
+    }
+  }
+}
