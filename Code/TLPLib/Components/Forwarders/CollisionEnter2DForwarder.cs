090b0d6e66c48ba5e6ea4f045b87dc88a23fa2a8
Arturas Slajus <x11@arturaz.net>
2014-05-18 18:30:44 +0300

    CollisionEnter2DForwarder

---
+++ Code/TLPLib/Components/Forwarders/CollisionEnter2DForwarder.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace com.tinylabproductions.TLPLib.Components.Forwarders {
+  public class CollisionEnter2DForwarder : MonoBehaviour {
+    public delegate void OnEnter(Collision2D collision);
+
+    public event OnEnter enter;
+
+    internal void OnCollisionEnter2D(Collision2D collision) {
+      if (enter == null) return;
+      enter(collision);
+    }
+  }
+}
