0693f69cf7c7dac5016f45632d35143908c6ee71
Arturas Slajus <arturas@tinylabproductions.com>
2014-05-23 15:58:03 +0300

    Don't init android unless on android.

--- Code/TLPLib/Android/AndroidActivity.cs
+++ Code/TLPLib/Android/AndroidActivity.cs
@@ -3,12 +3,17 @@ using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Android {
   public static class AndroidActivity {
-    private readonly static AndroidJavaClass unityPlayer = 
-      new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-    private readonly static AndroidJavaClass bridge =
-      new AndroidJavaClass("com.tinylabproductions.tlplib.Bridge");
-    public readonly static AndroidJavaObject current =
-      unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+    private static readonly AndroidJavaClass unityPlayer;
+    private static readonly AndroidJavaClass bridge;
+    public static readonly AndroidJavaObject current;
+    public static AndroidJavaObject activity { get { return current; } }
+    
+    static AndroidActivity() {
+      if (Application.isEditor) return;
+      unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+      bridge = new AndroidJavaClass("com.tinylabproductions.tlplib.Bridge");
+      current = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+    }
 
     public static void sharePNG(string path, string title, string sharerText) {
       bridge.CallStatic("sharePNG", path, title, sharerText);
