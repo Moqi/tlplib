30c784d12a0fc016e186a55baca0739668e4ac58
Arturas Slajus <arturas@tinylabproductions.com>
2014-05-26 17:23:18 +0300

    FPS class.

--- Code/TLPLib/Components/HUDFPS.cs
+++ Code/TLPLib/Components/HUDFPS.cs
@@ -1,5 +1,5 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using com.tinylabproductions.TLPLib.Utilities;
+using UnityEngine;
 
 [AddComponentMenu("Utilities/HUDFPS")]
 public class HUDFPS : MonoBehaviour
@@ -18,43 +18,19 @@ public class HUDFPS : MonoBehaviour
   public Rect startRect = new Rect(10, 10, 75, 50); // The rect the window is initially displayed at.
   public bool updateColor = true; // Do you want the color to change if the FPS gets low
   public bool allowDrag = true; // Do you want to allow the dragging of the FPS window
-  public float frequency = 0.5F; // The update frequency of the fps
   public int nbDecimal = 1; // How many decimal do you want to display
 
-  private float accum = 0f; // FPS accumulated over the interval
-  private int frames = 0; // Frames drawn over the interval
   private Color color = Color.white; // The color of the GUI, depending of the FPS ( R < 10, Y < 30, G >= 30 )
   private string sFPS = ""; // The fps formatted into a string.
   private GUIStyle style; // The style the text will be displayed at, based en defaultSkin.label.
 
-  void Start()
-  {
-    StartCoroutine(FPS());
-  }
-
-  void Update()
-  {
-    accum += Time.timeScale / Time.deltaTime;
-    ++frames;
-  }
-
-  IEnumerator FPS()
-  {
-    // Infinite loop executed every "frenquency" secondes.
-    while (true)
-    {
-      // Update the FPS
-      float fps = accum / frames;
+  void Start() {
+    FPS.fps.subscribe(fps => {
       sFPS = fps.ToString("f" + Mathf.Clamp(nbDecimal, 0, 10));
 
       //Update the color
       color = (fps >= 30) ? Color.green : ((fps > 10) ? Color.red : Color.yellow);
-
-      accum = 0.0F;
-      frames = 0;
-
-      yield return new WaitForSeconds(frequency);
-    }
+    });
   }
 
   void OnGUI()
