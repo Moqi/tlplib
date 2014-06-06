8eff440cc964f448e179a3976c3c6e3e9f8cf5f6
Arturas Slajus <arturas@tinylabproductions.com>
2014-06-06 11:37:58 +0300

    Improve lazy logging - show in editor/debug builds and don't allocated closures.

--- Assets/Vendor/TLPLib/Logger/Log.cs
+++ Assets/Vendor/TLPLib/Logger/Log.cs
@@ -3,15 +3,12 @@ using UnityEngine;
 
 namespace com.tinylabproductions.TLPLib.Logger {
   public static class Log {
-    public const bool isDebug = 
-#if UNITY_EDITOR
-      true;
-#else
-      false;
-#endif
+    public static readonly bool isDebug = Application.isEditor || Debug.isDebugBuild;
 
     public static void debug(object o) { if (isDebug) Debug.Log("[DEBUG]> " + o); }
-    public static void debug(Fn<object> o) { if (isDebug) debug(o()); }
+    // Lazy debug with context. Context is required to not allocate a closure
+    // for enclosed variables.
+    public static void debug<Ctx>(Ctx ctx, Fn<Ctx, object> o) { if (isDebug) debug(o(ctx)); }
     public static void info(object o) { Debug.LogWarning("[INFO]> " + o); }
     public static void warn(object o) { Debug.LogWarning("[WARN]> " + o); }
     public static void error(Exception ex) { Debug.LogException(ex); }
