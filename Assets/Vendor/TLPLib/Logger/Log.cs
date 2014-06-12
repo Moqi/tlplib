50264da3f2b5cdef741d84513d4378c9eeae5de7
Arturas Slajus <arturas@tinylabproductions.com>
2014-06-12 17:57:48 +0300

    Logging to local file from unity editor.

--- Assets/Vendor/TLPLib/Logger/Log.cs
+++ Assets/Vendor/TLPLib/Logger/Log.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using UnityEngine;
+using Debug = UnityEngine.Debug;
 
 namespace com.tinylabproductions.TLPLib.Logger {
   public static class Log {
@@ -13,5 +16,23 @@ namespace com.tinylabproductions.TLPLib.Logger {
     public static void warn(object o) { Debug.LogWarning("[WARN]> " + o); }
     public static void error(Exception ex) { Debug.LogException(ex); }
     public static void error(object o) { Debug.LogError("[ERROR]> " + o); }
+
+#if UNITY_EDITOR
+    public static readonly string logfilePath;
+    public static readonly StreamWriter logfile;
+
+    static Log() {
+      logfilePath = Application.temporaryCachePath + "/unity-editor-runtime.log";
+      info("Editor Runtime Logfile: " + logfilePath);
+      logfile = new StreamWriter(
+        File.Open(logfilePath, FileMode.Append, FileAccess.Write, FileShare.Read)
+      ) { AutoFlush = true };
+
+      editor("\n\nLog opened at " + DateTime.Now + "\n\n");
+    }
+#endif
+
+    [Conditional("UNITY_EDITOR")]
+    public static void editor(object o) { logfile.WriteLine(o); }
   }
 }
