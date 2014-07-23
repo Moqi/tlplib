50bdbf783f97b68a1b8d0fd9d94b1393f71e64b6
Arturas Slajus <arturas@tinylabproductions.com>
2014-07-23 14:19:54 +0300

    RandomList collection.

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Collection/Editor/RandomListTest.cs
@@ -0,0 +1,153 @@
+﻿#if UNITY_TEST
+using System;
+using com.tinylabproductions.TLPLib.Functional;
+using NUnit.Framework;
+
+namespace com.tinylabproductions.TLPLib.Collection.Editor {
+  [TestFixture]
+  class RandomListTest {
+    #region #Insert
+
+    [Test] public void InsertStartTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      l.Insert(0, 10);
+      Assert.AreEqual(F.list(10, 2, 3, 4, 1), l);
+    }
+
+    [Test] public void InsertMiddleTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      l.Insert(2, 10);
+      Assert.AreEqual(F.list(1, 2, 10, 4, 3), l);
+    }
+
+    [Test] public void InsertEndTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      l.Insert(4, 10);
+      Assert.AreEqual(F.list(1, 2, 3, 4, 10), l);
+    }
+
+    [Test] public void InsertOverEndTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      Assert.Throws<ArgumentOutOfRangeException>(() => l.Insert(5, 10));
+    }
+
+    #endregion
+
+    #region #Remove
+
+    [Test] public void RemoveFromStartTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      Assert.AreEqual(true, l.Remove(1));
+      Assert.AreEqual(F.list(4, 2, 3), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    [Test] public void RemoveFromMiddleTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      Assert.AreEqual(true, l.Remove(2));
+      Assert.AreEqual(F.list(1, 4, 3), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    [Test] public void RemoveFromEndTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      Assert.AreEqual(true, l.Remove(4));
+      Assert.AreEqual(F.list(1, 2, 3), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    [Test] public void RemoveNonExistingTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      Assert.AreEqual(false, l.Remove(5));
+      Assert.AreEqual(F.list(1, 2, 3, 4), l);
+      Assert.AreEqual(4, l.Count);
+    }
+
+    [Test] public void RemoveDuplicateTest() {
+      var l = new RandomList<int> {1, 2, 2, 3, 4};
+      Assert.AreEqual(true, l.Remove(2));
+      Assert.AreEqual(F.list(1, 4, 2, 3), l);
+      Assert.AreEqual(4, l.Count);
+      Assert.AreEqual(true, l.Remove(2));
+      Assert.AreEqual(F.list(1, 4, 3), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    #endregion
+
+    #region #RemoveAt
+
+    [Test] public void RemoveAtFromStartTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      l.RemoveAt(0);
+      Assert.AreEqual(F.list(4, 2, 3), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    [Test] public void RemoveAtFromMiddleTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      l.RemoveAt(2);
+      Assert.AreEqual(F.list(1, 2, 4), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    [Test] public void RemoveAtFromEndTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      l.RemoveAt(3);
+      Assert.AreEqual(F.list(1, 2, 3), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    [Test] public void RemoveAtNonExistingTest() {
+      var l = new RandomList<int> {1, 2, 3, 4};
+      Assert.Throws<ArgumentOutOfRangeException>(() => l.RemoveAt(5));
+      Assert.AreEqual(F.list(1, 2, 3, 4), l);
+      Assert.AreEqual(4, l.Count);
+    }
+
+    #endregion
+
+    #region #RemoveWhere
+
+    [Test] public void RemoveWhereSequentialTest() {
+      var l = new RandomList<int> { 1, 2, 2, 2, 3, 4, 4, 4, 5 };
+      l.RemoveWhere(i => i % 2 == 0);
+      // {1, 5, 2, 2, 3, 4, 4, 4}
+      // {1, 5, 4, 2, 3, 4, 4}
+      // {1, 5, 4, 2, 3, 4}
+      // {1, 5, 4, 2, 3}
+      // {1, 5, 3, 2}
+      // {1, 5, 3}
+      Assert.AreEqual(F.list(1, 5, 3), l);
+      Assert.AreEqual(3, l.Count);
+    }
+
+    [Test] public void RemoveWhereAlteringTest() {
+      var l = new RandomList<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+      l.RemoveWhere(i => i % 2 == 0);
+      // { 1, 9, 3, 4, 5, 6, 7, 8 }
+      // { 1, 9, 3, 8, 5, 6, 7 }
+      // { 1, 9, 3, 7, 5, 6 }
+      // { 1, 9, 3, 7, 5 }
+      Assert.AreEqual(F.list(1, 9, 3, 7, 5), l);
+      Assert.AreEqual(5, l.Count);
+    }
+
+    [Test] public void RemoveWhereAllTest() {
+      var l = new RandomList<int> { 2, 4, 6, 8, 10 };
+      l.RemoveWhere(i => i % 2 == 0);
+      Assert.AreEqual(F.emptyList<int>(), l);
+      Assert.AreEqual(0, l.Count);
+    }
+
+    [Test] public void RemoveWhereNoneTest() {
+      var l = new RandomList<int> { 1, 3, 5, 7, 9 };
+      l.RemoveWhere(i => i % 2 == 0);
+      Assert.AreEqual(F.list(1, 3, 5, 7, 9), l);
+      Assert.AreEqual(5, l.Count);
+    }
+
+    #endregion
+  }
+}
+#endif
---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Collection/RandomList.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace com.tinylabproductions.TLPLib.Collection {
+  /* A list which has random order inside. 
+   * 
+   * Insertions: O(1), unless buffer expansion is needed.
+   * Removals: O(1).
+   * Traversals: O(n).
+   */
+  public class RandomList<A> : IList<A> {
+    private readonly List<A> backing = new List<A>();
+
+    public void Add(A item) { backing.Add(item); }
+    public void Clear() { backing.Clear(); }
+    public bool Contains(A item) { return backing.Contains(item); }
+    public void CopyTo(A[] array, int arrayIndex) { backing.CopyTo(array, arrayIndex); }
+    public int IndexOf(A item) { return backing.IndexOf(item); }
+    public int Count { get { return backing.Count; } }
+    public bool IsReadOnly { get { return false; } }
+    public IEnumerator<A> GetEnumerator() { return backing.GetEnumerator(); }
+    IEnumerator IEnumerable.GetEnumerator() { return GetEnumerator(); }
+    public A this[int index] {
+      get { return backing[index]; }
+      set { backing[index] = value; }
+    }
+
+    /* Insert element at index, moving current element to the end if it exists. */
+    public void Insert(int index, A item) {
+      // Delegate end and over the end insertions to backing.
+      if (index >= backing.Count) backing.Insert(index, item);
+      else {
+        var current = backing[index];
+        backing[index] = item;
+        backing.Add(current);
+      }
+    }
+
+    /* Removes first occurence of element, moving last element to its position. */
+    public bool Remove(A item) {
+      var index = backing.IndexOf(item);
+      if (index == -1) return false;
+      RemoveAt(index);
+      return true;
+    }
+
+    /* Removes element at index, moving last element to its position. */
+    public void RemoveAt(int index) {
+      var lastIndex = backing.Count - 1;
+      if (index == lastIndex) {
+        backing.RemoveAt(lastIndex);
+      }
+      else {
+        backing[index] = backing[lastIndex];
+        backing.RemoveAt(lastIndex);
+      }
+    }
+
+    /* Removes elements where predicate returns true. */
+    public void RemoveWhere(Fn<A, bool> predicate) {
+      var idx = 0;
+      while (idx < Count) {
+        var item = backing[idx];
+        if (predicate(item)) RemoveAt(idx);
+        else idx++;
+      }
+    }
+  }
+}
