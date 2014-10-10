9b7035c8d15b961045f409c4a55b9aa246fcd110
Arturas Slajus <arturas@tinylabproductions.com>
2014-10-10 12:53:32 +0300

    public static Fn<A, C> andThen<A, B, C>(this Fn<A, B> f1, Fn<B, C> f2)

--- parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/FuncExts.cs
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Extensions/FuncExts.cs
@@ -5,5 +5,9 @@ namespace com.tinylabproductions.TLPLib.Extensions {
     public static Act<A> andThen<A, B>(this Fn<A, B> f, Act<B> a) {
       return value => a(f(value));
     }
+
+    public static Fn<A, C> andThen<A, B, C>(this Fn<A, B> f1, Fn<B, C> f2) {
+      return value => f2(f1(value));
+    }
   }
 }
