62bfefe869ab4cf4ef86d9224b3cf8a7044b7c32
Arturas Slajus <arturas@tinylabproductions.com>
2014-10-16 14:57:52 +0300

    Size2

---
+++ parts/0000-TLPLib/Assets/Vendor/TLPLib/Data/Size2.cs
@@ -0,0 +1,15 @@
+﻿namespace com.tinylabproductions.TLPLib.Data {
+  public struct Size2 {
+    public readonly int x, y;
+
+    public Size2(int x, int y) {
+      this.x = x;
+      this.y = y;
+    }
+
+    public int index(int x, int y) { return x + y * this.x; }
+    public int linearSize { get { return x * y; } }
+
+    public override string ToString() { return string.Format("{0}x{1}", x, y); }
+  }
+}
