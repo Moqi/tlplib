4d379a236692f3617dbb795f73d20bcdc02c6458
Arturas Slajus <arturas@tinylabproductions.com>
2014-10-23 15:01:21 +0300

    Log to file when running in debug mode.

--- TLPLib/Logger/Log.cs
+++ TLPLib/Logger/Log.cs
@@ -1,36 +1,61 @@
 ﻿using System;
 using System.Diagnostics;
 using System.IO;
+using com.tinylabproductions.TLPLib.Extensions;
 using UnityEngine;
 using Debug = UnityEngine.Debug;
 
 namespace com.tinylabproductions.TLPLib.Logger {
   public static class Log {
-    [Conditional("UNITY_EDITOR"), Conditional("DEBUG")]
-    public static void debug(object o) { Debug.Log("[DEBUG]> " + o); }
-    public static void info(object o) { Debug.LogWarning("[INFO]> " + o); }
-    public static void warn(object o) { Debug.LogWarning("[WARN]> " + o); }
-    public static void error(Exception ex) { Debug.LogException(ex); }
-    public static void error(object o) { Debug.LogError("[ERROR]> " + o); }
-    [Conditional("UNITY_EDITOR")]
-    public static void editor(object o) { EditorLog.log(o); }
+    [Conditional("DEBUG")]
+    public static void debug(object o) {
+      var str = "[DEBUG]> " + o;
+      Debug.Log(str);
+      file(str);
+    }
+
+    public static void info(object o) {
+      var str = "[INFO]> " + o;
+      Debug.LogWarning(str);
+      file(str);
+    }
+
+    public static void warn(object o) {
+      var str = "[WARN]> " + o;
+      Debug.LogWarning(str);
+    }
+
+    public static void error(Exception ex) {
+      Debug.LogException(ex);
+      file("[ERROR]> Exception!");
+      file(ex.ToString());
+    }
+
+    public static void error(object o) {
+      var str = "[ERROR]> " + o;
+      Debug.LogError(str);
+      file(str);
+    }
+
+    [Conditional("DEBUG")]
+    public static void file(object o) { FileLog.log(o); }
   }
 
-  class EditorLog {
+  class FileLog {
     public static readonly string logfilePath;
     public static readonly StreamWriter logfile;
     
-    static EditorLog() {
-      logfilePath = Application.temporaryCachePath + "/unity-editor-runtime.log";
-      Log.info("Editor Runtime Logfile: " + logfilePath);
+    static FileLog() {
+      logfilePath = Application.temporaryCachePath + "/runtime.log";
       logfile = new StreamWriter(
         File.Open(logfilePath, FileMode.Append, FileAccess.Write, FileShare.Read)
       ) { AutoFlush = true };
 
+      Log.info("Runtime Logfile: " + logfilePath);
       log("\n\nLog opened at " + DateTime.Now + "\n\n");
     }
 
-    [Conditional("UNITY_EDITOR")]
+    [Conditional("DEBUG")]
     public static void log(object o) { logfile.WriteLine(o); }
   }
 }
