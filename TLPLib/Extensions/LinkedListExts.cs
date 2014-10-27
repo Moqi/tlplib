9bed66f4922feac1c4ba50b325756bf03f957420
Arturas Slajus <x11@arturaz.net>
2014-10-27 17:13:18 +0200

    Fix LinkedList#shift and #pop, added #iterateWhileChangedN, #iterateWhileChanged, #removeWhere

--- TLPLib/Extensions/LinkedListExts.cs
+++ TLPLib/Extensions/LinkedListExts.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using com.tinylabproductions.TLPLib.Collection;
 using com.tinylabproductions.TLPLib.Functional;
 
@@ -12,16 +13,38 @@ namespace com.tinylabproductions.TLPLib.Extensions {
 
     /* Removes first element and returns it. */
     public static Option<A> shift<A>(this LinkedList<A> list) {
-      var a = F.opt(list.First);
-      list.RemoveFirst();
-      return a.map(_ => _.Value);
+      return F.opt(list.First).map(n => {
+        list.RemoveFirst();
+        return n.Value;
+      });
     }
 
     /* Removes last element and returns it. */
     public static Option<A> pop<A>(this LinkedList<A> list) {
-      var a = F.opt(list.Last);
-      list.RemoveLast();
-      return a.map(_ => _.Value);
+      return F.opt(list.Last).map(n => {
+        list.RemoveLast();
+        return n.Value;
+      });
+    }
+
+    /* As foreach, but does not freak out if underlying list is changed. */
+    public static void iterateWhileChangedN<A>(this LinkedList<A> list, Act<LinkedListNode<A>> elem) {
+      var node = list.First;
+      while (node != null) {
+        elem(node);
+        node = node.Next;
+      }
+    }
+
+    /* As foreach, but does not freak out if underlying list is changed. */
+    public static void iterateWhileChanged<A>(this LinkedList<A> list, Act<A> elem) {
+      list.iterateWhileChangedN(node => elem(node.Value));
+    }
+
+    public static void removeWhere<A>(this LinkedList<A> list, Fn<A, bool> predicate) {
+      list.iterateWhileChangedN(node => {
+        if (predicate(node.Value)) list.Remove(node);
+      });
     }
   }
 }
