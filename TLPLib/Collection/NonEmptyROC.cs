f65a7d18513f5f8f94e58d7e6e75c8ba661e64a9
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-03 14:00:13 +0200

    NonEmptyROC - Non-empty Read-Only Collection

---
+++ TLPLib/Collection/NonEmptyROC.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using com.tinylabproductions.TLPLib.Extensions;
+using com.tinylabproductions.TLPLib.Functional;
+
+namespace com.tinylabproductions.TLPLib.Collection {
+  public static class NonEmptyROC {
+    public static Option<NonEmptyROC<A>> a<A>(IList<A> list) 
+    { return NonEmptyROC<A>.a(list); }
+  }
+
+  /* Non-empty Read-Only Collection */
+  public class NonEmptyROC<A> : ReadOnlyCollection<A> {
+    private NonEmptyROC(IList<A> list) : base(list) { }
+
+    public static Option<NonEmptyROC<A>> a(IList<A> list) 
+    { return (!list.isEmpty()).opt(new NonEmptyROC<A>(list)); }
+  }
+}
