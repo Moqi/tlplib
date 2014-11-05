91343a04bf6054b29669f5b4c67f95075a66bac8
Arturas Slajus <x11@arturaz.net>
2014-11-05 12:48:09 +0200

    Alternate constructor for NonEmptyROC

--- TLPLib/Collection/NonEmptyROC.cs
+++ TLPLib/Collection/NonEmptyROC.cs
@@ -5,6 +5,9 @@ using com.tinylabproductions.TLPLib.Functional;
 
 namespace com.tinylabproductions.TLPLib.Collection {
   public static class NonEmptyROC {
+    public static NonEmptyROC<A> a<A>(A first, params A[] rest) 
+    { return NonEmptyROC<A>.a(first, rest); }
+
     public static Option<NonEmptyROC<A>> a<A>(IList<A> list) 
     { return NonEmptyROC<A>.a(list); }
   }
@@ -12,6 +15,13 @@ namespace com.tinylabproductions.TLPLib.Collection {
   /* Non-empty Read-Only Collection */
   public class NonEmptyROC<A> : ReadOnlyCollection<A> {
     private NonEmptyROC(IList<A> list) : base(list) { }
+
+    public static NonEmptyROC<A> a(A first, params A[] rest) {
+      var arr = new A[rest.Length + 1];
+      arr[0] = first;
+      rest.CopyTo(arr, 1);
+      return new NonEmptyROC<A>(arr);
+    }
 
     public static Option<NonEmptyROC<A>> a(IList<A> list) 
     { return (!list.isEmpty()).opt(new NonEmptyROC<A>(list)); }
