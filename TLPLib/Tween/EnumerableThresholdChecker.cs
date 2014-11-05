d97c40163aaa347c46fca7f92afec5b2f41847fb
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-05 17:15:49 +0200

    ThresholdCheckers.

---
+++ TLPLib/Tween/EnumerableThresholdChecker.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using com.tinylabproductions.TLPLib.Functional;
+using com.tinylabproductions.TLPLib.Logger;
+
+namespace com.tinylabproductions.TLPLib.Tween {
+  /* Threshold checker that iterates through given enumerable when threshold 
+   * is met. */
+  public static class EnumerableThresholdChecker {
+    public delegate void ThresholdReached<in A>(int index, A a);
+
+    public static EnumerableThresholdChecker<A> a<A>(
+      IEnumerable<A> enumerable, Fn<A, float> toThreshold,
+      ThresholdChecker.CheckThreshold checkerFn,
+      ThresholdReached<A> onThresholdReached
+    ) {
+      return new EnumerableThresholdChecker<A>(enumerable, toThreshold, checkerFn, onThresholdReached);
+    }
+  }
+
+  public class EnumerableThresholdChecker<A> {
+    private readonly IEnumerator<A> enumerator;
+    private readonly Fn<A, float> toThreshold;
+    private readonly ThresholdChecker.CheckThreshold checkerFn;
+    private readonly EnumerableThresholdChecker.ThresholdReached<A> onThresholdReached;
+
+    private Option<ThresholdChecker> checker;
+    private int index = -1;
+
+    public EnumerableThresholdChecker(
+      IEnumerable<A> enumerable, Fn<A, float> toThreshold,
+      ThresholdChecker.CheckThreshold checkerFn,
+      EnumerableThresholdChecker.ThresholdReached<A> onThresholdReached
+    ) {
+      enumerator = enumerable.GetEnumerator();
+      this.toThreshold = toThreshold;
+      this.checkerFn = checkerFn;
+      this.onThresholdReached = onThresholdReached;
+      checker = F.none<ThresholdChecker>();
+      next();
+    }
+
+    private void next() {
+      if (enumerator.MoveNext()) {
+        index++;
+        var a = enumerator.Current;
+        Log.debug("Threshold reached, switching to next element: " + a);
+        checker = F.some(new ThresholdChecker(
+          () => toThreshold(a), checkerFn, () => {
+            onThresholdReached(index, a);
+            next();
+          }
+        ));
+      }
+      else {
+        checker = F.none<ThresholdChecker>();
+      }
+    }
+
+    public void check() { checker.each(_ => _.check()); }
+  }
+}
---
+++ TLPLib/Tween/ThresholdChecker.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace com.tinylabproductions.TLPLib.Tween {
+  public struct ThresholdChecker {
+    public delegate float GetThreshold();
+    public delegate bool CheckThreshold(float last, float current);
+
+    public static CheckThreshold minimize = (last, cur) => last > cur;
+    public static CheckThreshold maximize = (last, cur) => last < cur;
+
+    private readonly CheckThreshold checkThreshold;
+    private readonly GetThreshold getThreshold;
+    private readonly Act onThresholdReached;
+
+    private float threshold;
+
+    public ThresholdChecker(
+      GetThreshold getThreshold, CheckThreshold checkThreshold,
+      Act onThresholdReached
+    ) {
+      this.getThreshold = getThreshold;
+      this.checkThreshold = checkThreshold;
+      this.onThresholdReached = onThresholdReached;
+      threshold = getThreshold();
+    }
+
+    public void check() {
+      var curThreshold = getThreshold();
+      if (checkThreshold(threshold, curThreshold)) onThresholdReached();
+      threshold = curThreshold;
+    }
+  }
+}
