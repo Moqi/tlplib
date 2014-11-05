b0d1f928a8929f18d97642cfb6ac0fc5dd0dc9ee
Arturas Slajus <x11@arturaz.net>
2014-11-05 12:48:17 +0200

    TweenUtil.withTween

---
+++ TLPLib/Tween/TweenUtil.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using com.tinylabproductions.TLPLib.Functional;
+
+namespace com.tinylabproductions.TLPLib.Tween {
+  public static class TweenUtil {
+    public delegate void SetCurrentTween(AbstractGoTween tween);
+
+    /* Allows you to have only one running tween per closure. */
+    public static void withTween(Act<SetCurrentTween> runTween) {
+      var current = F.none<AbstractGoTween>();
+      runTween(tween => {
+        current.each(_ => _.destroy());
+        current = F.some(tween);
+      });
+    }
+  }
+}
