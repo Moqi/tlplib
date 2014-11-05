142768b207b6c402e462ac1f61a12fd12f17ba41
Arturas Slajus <arturas@tinylabproductions.com>
2014-11-05 17:14:57 +0200

    NonEmptyROC2

--- TLPLib/Collection/NonEmptyROC.cs
+++ TLPLib/Collection/NonEmptyROC.cs
@@ -8,7 +8,7 @@ namespace com.tinylabproductions.TLPLib.Collection {
     public static NonEmptyROC<A> a<A>(A first, params A[] rest) 
     { return NonEmptyROC<A>.a(first, rest); }
 
-    public static Option<NonEmptyROC<A>> a<A>(IList<A> list) 
+    public static Option<NonEmptyROC<A>> fromList<A>(IList<A> list) 
     { return NonEmptyROC<A>.a(list); }
   }
 
@@ -24,6 +24,40 @@ namespace com.tinylabproductions.TLPLib.Collection {
     }
 
     public static Option<NonEmptyROC<A>> a(IList<A> list) 
-    { return (!list.isEmpty()).opt(new NonEmptyROC<A>(list)); }
+    { return (!list.isEmpty()).opt(() => new NonEmptyROC<A>(list)); }
+  }
+
+  public static class NonEmptyROC2 {
+    public static NonEmptyROC2<A> a<A>(A first, A second, params A[] rest) 
+    { return NonEmptyROC2<A>.a(first, second, rest); }
+
+    public static NonEmptyROC2<A> a<A>(A first, NonEmptyROC<A> rest) 
+    { return NonEmptyROC2<A>.a(first, rest); }
+
+    public static Option<NonEmptyROC2<A>> fromList<A>(IList<A> list) 
+    { return NonEmptyROC2<A>.a(list); }
+  }
+
+  /* Non-empty Read-Only Collection with at least 2 values*/
+  public class NonEmptyROC2<A> : ReadOnlyCollection<A> {
+    private NonEmptyROC2(IList<A> list) : base(list) { }
+
+    public static NonEmptyROC2<A> a(A first, A second, params A[] rest) {
+      var arr = new A[rest.Length + 2];
+      arr[0] = first;
+      arr[1] = second;
+      rest.CopyTo(arr, 2);
+      return new NonEmptyROC2<A>(arr);
+    }
+
+    public static NonEmptyROC2<A> a(A first, NonEmptyROC<A> rest) {
+      var arr = new A[rest.Count + 1];
+      arr[0] = first;
+      rest.CopyTo(arr, 1);
+      return new NonEmptyROC2<A>(arr);
+    }
+
+    public static Option<NonEmptyROC2<A>> a(IList<A> list) 
+    { return (list.Count > 1).opt(() => new NonEmptyROC2<A>(list)); }
   }
 }
